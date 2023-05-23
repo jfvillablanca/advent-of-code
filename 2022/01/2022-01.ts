d69933759228fe1ebd28716e3f2cc8431f4e1168
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-23 21:09:47 +0800

    TS: untidy working solution

--- 2022/01/2022-01.ts
+++ 2022/01/2022-01.ts
@@ -4,13 +4,35 @@ import path from "path";
 const inputFileName = "2022-01-input.txt";
 
 const logic = (input: string) => {
-    const arr = Array.from(input.split("\n"));
-    console.log(arr)
+    const elfArr = [];
+    const inputArr = [...input.split("\n")].map((x) =>
+        x === "" ? "X" : Number(x)
+    );
+
+    let tempArr: number[] = [];
+    for (const num of inputArr) {
+        if (num === "X") {
+            elfArr.push(tempArr.reduce((acc, cur) => acc + cur, 0));
+            tempArr = [];
+        } else {
+            tempArr.push(num);
+        }
+    }
+    console.log(elfArr.sort((a, b) => b - a)[0]);
+    console.log(
+        elfArr
+            .sort((a, b) => b - a)
+            .slice(0, 3)
+            .reduce((acc, cur) => acc + cur, 0)
+    );
 };
 
 const main = async () => {
     try {
-        const input = await fs.promises.readFile(path.join(__dirname, inputFileName), "utf8");
+        const input = await fs.promises.readFile(
+            path.join(__dirname, inputFileName),
+            "utf8"
+        );
         logic(input);
     } catch (err) {
         console.error(err);
