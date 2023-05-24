54da340b6a3c34ed4410ee4ab0100946fae6efdf
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-24 15:59:32 +0800

    rust: working solution for day 1

--- src/bin/2022-01.rs
+++ src/bin/2022-01.rs
@@ -1,11 +1,41 @@
 use std::fs::File;
-use std::io::{BufReader, Error};
+use std::io::{BufRead, BufReader, Error};
 use std::{env, println};
 
 const INPUT_FILENAME: &str = "2022-01-input.txt";
 
 fn logic(buffered: BufReader<File>) {
-    println!("{:?}", buffered)
+    let mut output_arr: Vec<i32> = buffered
+        .lines()
+        .filter_map(|unparsed_number| unparsed_number.ok())
+        .fold(Vec::new(), |mut acc: Vec<i32>, cur| {
+            if cur.is_empty() {
+                acc.push(0);
+            } else {
+                if let Some(last) = acc.last_mut() {
+                    *last += if let Ok(parsed_number) = cur.parse::<i32>() {
+                        parsed_number
+                    } else {
+                        0
+                    };
+                }
+            }
+            acc
+        });
+    // output_arr.sort(); // sort in ascending order
+    output_arr.sort_by(|a, b| b.cmp(a));
+
+    // println!("Highest total num: {}", output_arr.last().unwrap_or(&0)); // if sorted in ascending order
+    println!("Highest total num: {}", output_arr[0]);
+    println!(
+        "Total sum of top 3: {}",
+        // output_arr[output_arr.len() - 3..] // if sorted in ascending order
+        output_arr[..3]
+            .iter()
+            .map(|&x| x)
+            .reduce(|acc, cur| acc + cur)
+            .unwrap_or(0)
+    );
 }
 
 fn main() -> Result<(), Error> {
