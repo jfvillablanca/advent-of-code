16c32f5f21a1924a1f090f60af1d6fc591e591dc
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-25 13:21:15 +0800

    rust: rewrite to use struct instead of tuple

--- src/bin/2022-02.rs
+++ src/bin/2022-02.rs
@@ -4,84 +4,91 @@ use std::{env, println};
 
 const INPUT_FILENAME: &str = "2022-02-input.txt";
 
+struct StrategyScores {
+    strategy_one: i32,
+    strategy_two: i32,
+}
+
 fn logic(buffered: BufReader<File>) {
     let score = buffered
         .lines()
         .filter_map(|unparsed_line| unparsed_line.ok())
-        .fold((0, 0), |mut acc, cur| {
-            let mut collected_line = cur.split_whitespace();
-            let opponent = collected_line.next().unwrap_or("");
-            let yours = collected_line.next().unwrap_or("");
+        .fold(
+            StrategyScores {
+                strategy_one: 0,
+                strategy_two: 0,
+            },
+            |mut acc, cur| {
+                let mut collected_line = cur.split_whitespace();
+                let opponent = collected_line.next().unwrap_or("");
+                let yours = collected_line.next().unwrap_or("");
 
-            let mut acc_strategy_one = 0;
-            let mut acc_strategy_two = 0;
-
-            acc_strategy_one += match yours {
-                "X" => 1,
-                "Y" => 2,
-                _ => 3,
-            };
-            acc_strategy_one += if opponent == "A" {
-                // Rock
-                match yours {
-                    "Y" => 6,
-                    "X" => 3,
-                    _ => 0,
-                } // [Paper, Rock, Scissors]
-            } else if opponent == "B" {
-                // Paper
-                match yours {
-                    "Z" => 6,
-                    "Y" => 3,
-                    _ => 0,
-                } // [Scissors, Paper, Rock]
-            } else {
-                // Scissors
-                match yours {
-                    "X" => 6,
-                    "Z" => 3,
-                    _ => 0,
-                } // [Rock, Scissors, Paper]
-            };
-
-            acc_strategy_two += match yours {
-                "X" => 0,
-                "Y" => 3,
-                _ => 6,
-            };
-            acc_strategy_two += if opponent == "A" {
-                // Rock
-                match yours {
-                    "Y" => 1,
-                    "X" => 3,
-                    _ => 2,
-                } // [Rock, Scissors, Paper]
-            } else if opponent == "B" {
-                // Paper
-                match yours {
-                    "Y" => 2,
+                let acc_strategy_one = match yours {
                     "X" => 1,
+                    "Y" => 2,
                     _ => 3,
-                } // [Paper, Rock, Scissors]
-            } else {
-                // Scissors
-                match yours {
+                } + if opponent == "A" {
+                    // Rock
+                    match yours {
+                        "Y" => 6,
+                        "X" => 3,
+                        _ => 0,
+                    } // [Paper, Rock, Scissors]
+                } else if opponent == "B" {
+                    // Paper
+                    match yours {
+                        "Z" => 6,
+                        "Y" => 3,
+                        _ => 0,
+                    } // [Scissors, Paper, Rock]
+                } else {
+                    // Scissors
+                    match yours {
+                        "X" => 6,
+                        "Z" => 3,
+                        _ => 0,
+                    } // [Rock, Scissors, Paper]
+                };
+
+                let acc_strategy_two = match yours {
+                    "X" => 0,
                     "Y" => 3,
-                    "X" => 2,
-                    _ => 1,
-                } // [Scissors, Paper, Rock]
-            };
-            acc = (acc.0 + acc_strategy_one, acc.1 + acc_strategy_two);
-            acc
-        });
+                    _ => 6,
+                } + if opponent == "A" {
+                    // Rock
+                    match yours {
+                        "Y" => 1,
+                        "X" => 3,
+                        _ => 2,
+                    } // [Rock, Scissors, Paper]
+                } else if opponent == "B" {
+                    // Paper
+                    match yours {
+                        "Y" => 2,
+                        "X" => 1,
+                        _ => 3,
+                    } // [Paper, Rock, Scissors]
+                } else {
+                    // Scissors
+                    match yours {
+                        "Y" => 3,
+                        "X" => 2,
+                        _ => 1,
+                    } // [Scissors, Paper, Rock]
+                };
+                acc.strategy_one += acc_strategy_one;
+                acc.strategy_two += acc_strategy_two;
+                acc
+            },
+        );
 
     println!(
         "Part 1 | Total score based on strategy guide 1: {}",
-        score.0
+        score.strategy_one
     );
     println!(
         "Part 2 | Total score based on strategy guide 2: {}",
-        score.1
+        score.strategy_two
     );
 }
 
