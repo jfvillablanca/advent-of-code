4767c59f8d6839451c35774f679cfd9d0288ed07
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-25 13:09:22 +0800

    rust: working solution day2

--- src/bin/2022-02.rs
+++ src/bin/2022-02.rs
@@ -8,16 +8,18 @@ fn logic(buffered: BufReader<File>) {
     let score = buffered
         .lines()
         .filter_map(|unparsed_line| unparsed_line.ok())
-        .fold(0, |mut acc, cur| {
+        .fold((0, 0), |mut acc, cur| {
             let collected_line = cur.split(" ").collect::<Vec<&str>>();
             let (opponent, yours) = (collected_line[0], collected_line[1]);
+            let mut acc_strategy_one = 0;
+            let mut acc_strategy_two = 0;
 
-            acc += match yours {
+            acc_strategy_one += match yours {
                 "X" => 1,
                 "Y" => 2,
                 _ => 3,
             };
-            acc += if opponent == "A" {
+            acc_strategy_one += if opponent == "A" {
                 // Rock
                 match yours {
                     "Y" => 6,
@@ -33,19 +35,52 @@ fn logic(buffered: BufReader<File>) {
                 } // [Scissors, Paper, Rock]
             } else {
                 // Scissors
-                //
-
                 match yours {
                     "X" => 6,
                     "Z" => 3,
                     _ => 0,
                 } // [Rock, Scissors, Paper]
             };
+
+            acc_strategy_two += match yours {
+                "X" => 0,
+                "Y" => 3,
+                _ => 6,
+            };
+            acc_strategy_two += if opponent == "A" {
+                // Rock
+                match yours {
+                    "Y" => 1,
+                    "X" => 3,
+                    _ => 2,
+                } // [Rock, Scissors, Paper]
+            } else if opponent == "B" {
+                // Paper
+                match yours {
+                    "Y" => 2,
+                    "X" => 1,
+                    _ => 3,
+                } // [Paper, Rock, Scissors]
+            } else {
+                // Scissors
+                match yours {
+                    "Y" => 3,
+                    "X" => 2,
+                    _ => 1,
+                } // [Scissors, Paper, Rock]
+            };
+            acc = (acc.0 + acc_strategy_one, acc.1 + acc_strategy_two);
             acc
         });
 
-    println!("Part 1 | Total score based on strategy guide 1: {}", score);
-    // println!("Part 2 | Total score based on strategy guide 2: {}");
+    println!(
+        "Part 1 | Total score based on strategy guide 1: {}",
+        score.0
+    );
+    println!(
+        "Part 2 | Total score based on strategy guide 2: {}",
+        score.1
+    );
 }
 
 fn main() -> Result<(), Error> {
