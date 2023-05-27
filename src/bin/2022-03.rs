e9f4124126ebdf796f4da93aa7cc293652a8a70e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-27 19:33:07 +0800

    rust: working solution day3

--- src/bin/2022-03.rs
+++ src/bin/2022-03.rs
@@ -1,13 +1,13 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
-use std::{env, println};
+use std::{env, println, vec};
 
 const INPUT_FILENAME: &str = "2022-03-input.txt";
 
 struct PrioritySum {
     priority_by_compartment: i32,
-    // priority_by_elf_group: i32,
+    priority_by_elf_group: i32,
 }
 
 fn logic(buffered: BufReader<File>) {
@@ -18,6 +18,7 @@ fn logic(buffered: BufReader<File>) {
 
     let mut sum = PrioritySum {
         priority_by_compartment: 0,
+        priority_by_elf_group: 0,
     };
 
     let mut collector = Vec::new();
@@ -29,17 +30,28 @@ fn logic(buffered: BufReader<File>) {
         ])[0];
         sum.priority_by_compartment += map_char_to_value(common_elements);
 
+        if index % 3 == 0 && index != 0 {
+            let common_elements = find_common_elements(collector.clone())[0];
+            sum.priority_by_elf_group += map_char_to_value(common_elements);
+            collector.clear();
+        }
+        collector.push(current_line);
+    }
+    if !collector.is_empty() {
+        sum.priority_by_elf_group += map_char_to_value(find_common_elements(collector.clone())[0]);
+    }
+
     assert_eq!(sum.priority_by_compartment, 7553);
-    // assert_eq!(sum.priority_by_elf_group, 2758);
+    assert_eq!(sum.priority_by_elf_group, 2758);
 
     println!(
         "Part 1 | Sum of priorities between rucksack compartments: {}",
         sum.priority_by_compartment
     );
-    // println!(
-    //     "Part 2 | Sum of priorities between elf rucksack groups of 3: {}",
-    //     sum.priority_by_elf_group
-    // );
+    println!(
+        "Part 2 | Sum of priorities between elf rucksack groups of 3: {}",
+        sum.priority_by_elf_group
+    );
 }
 
 fn map_char_to_value(c: char) -> i32 {
