73ba448cd5e282333b794d2e84f904037f5c78ee
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-04-26 19:56:04 +0800

    go(2022): day1 part2

--- go/src/2022-01/main.go
+++ go/src/2022-01/main.go
@@ -25,7 +25,21 @@ func logic(scanner *bufio.Scanner) {
 		current, _ := strconv.Atoi(text)
 		accumulator += current
 	}
+
+	if accumulator != 0 {
+		outputArr = append(outputArr, accumulator)
+	}
+
 	fmt.Printf("Part 1 | Highest total num: %d\n", slices.Max(outputArr))
+
+	slices.Sort(outputArr)
+	slices.Reverse(outputArr)
+
+	accumulator = 0
+	for _, current := range outputArr[:3] {
+		accumulator += current
+	}
+	fmt.Printf("Part 2 | Total sum of top 3: %d\n", accumulator)
 }
 
 func main() {
